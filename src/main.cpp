6af48fd86cbb12a4c84bbad8dbb0a6a4af9a2575
huafengpei <hupei@cisco.com>
2025-11-20 22:26:38 +0800

    aaa

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@ void ConfigureChecker(sqlcheck::Configuration &state) {
 
 
  
-  
+
 
 
 
@@ -99,6 +99,12 @@ void ConfigureChecker(sqlcheck::Configuration &state) {
 }
 
 void Usage() {
+
+  char* test = new char[10];
+  delete []test;
+  strcpy(test, "aaaa");
+
+
   std::cout <<
       "Command line options : sqlcheck <options>\n"
       "   -f -file_name          :  SQL file name\n"
