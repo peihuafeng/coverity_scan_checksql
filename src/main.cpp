b3aee7a268cd987fab13b0460b5cef7c1e242463
huafengpei <hupei@cisco.com>
2025-11-21 15:47:14 +0800

    test not skip checks
    request-checks: true

--- src/main.cpp
+++ src/main.cpp
@@ -122,7 +122,9 @@ void Usage() {
 int main(int argc, char **argv) {
 
   bool has_issues = false;
-
+  char* test = new char[10];
+  delete []test;
+  strcpy(test, "aaaa");
   try {
 
     // Parse the input arguments from the user
